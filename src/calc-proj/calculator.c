742fd70cd187b4ea123152922242aa8f8bb80452
Jakub <jakubmigac16@gmail.com>
2022-09-20 16:04:38 +0000

    mode check implement

--- src/calc-proj/calculator.c
+++ src/calc-proj/calculator.c
@@ -8,29 +8,35 @@ void calc()
     int ModeSelect;
     int inputA;
     int inputB;
-    static int modearray[3];
+    int checker;
+    int result;
 
-    printf("Select mode:  '\n' 1 '\n' 2'\n' 3'");
-    scanf(ModeSelect);
+    scanf("Select a mode: ");
+    printf("Select mode:  '\n' 1: Addition'\n' 2: Subtractions'\n' 3: Multiplication' '\n'");
+
+
+    scanf("%d %d", &inputA, &inputB);
 
     //MODE SELECTION
-    if (modearray[3] == 3){
-        int result = inputA + inputB;
-        return result;
+    if (checker == 3){
+        result = inputA + inputB;
+        printf("The Result is: " + result);
+        
     }
-        if (modearray[3] == 2){
+        if (checker == 2){
         int result = inputA - inputB;
-        return result;
+        printf("The Result is: " + result);
     }
-        if (modearray[3] == 1){
+        if (checker == 1){
         int result = inputA * inputB;
-        return result;
+        printf("The Result is: " + result);       
+    }
+
+    else if (checker != 1,2,3)
+    {
+        __THROW(printf("Error: Wrong input given try again"));
     }
 }
-
-
-
-
 
 int main(){
     calc();
