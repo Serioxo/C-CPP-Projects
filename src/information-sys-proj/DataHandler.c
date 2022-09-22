1eabd7a1ef51b760d938bdd7e5b703625a63b347
Jakub <jakubmigac16@gmail.com>
2022-09-22 10:51:44 +0000

    Data Handler project started

---
+++ src/information-sys-proj/DataHandler.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+//Shows main menu
+void mainmenu(){
+    
+}
+// Adds client to system
+void addClient(){
+
+}
+// Removes client from system
+void removeClient(){
+
+}
+// Edit client info
+void editClient(){
+
+}
+// View client info
+void viewClient(){
+
+}
+// Get data about client
+int getData(){
+
+}
+// Call viewClient via ID
+void checkClientID(int ID){
+
+}
